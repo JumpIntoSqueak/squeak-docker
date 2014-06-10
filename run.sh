9635945c7f4833363ae55173e4d9aa5d494be7f8
Robert Lehmann <mail@robertlehmann.de>
2014-06-10 17:41:41 +0200

    Fix up latest file changes, #2.

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 Xvfb -screen 0 1024x768x16 -ac &
 sleep 5
 websockify -D 5900 -- x11vnc -forever -usepw -display :0 &
-./wait_monticello_commit.sh &
 DISPLAY=:0 ./Squeak-$VERSION-All-in-One.app/Contents/Linux-i686/bin/squeak -vm-sound-null ./Squeak-$VERSION-All-in-One.app/Contents/Resources/Squeak*.image /run.st
