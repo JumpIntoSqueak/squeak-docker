53109260af6d588438b7f3c268e1349e9faf0db3
Hubert Hesse <hubert.hesse@hubertscorner.de>
2014-08-14 20:54:10 +0200

    use vnc(5900) and websockified(80) for noVNC
    the user can decide which one to use via `docker run -p 80:hostport`

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 Xvfb -screen 0 1024x768x16 -ac &
 sleep 5
-websockify -D 5900 -- x11vnc -forever -usepw -display :0 &
+x11vnc -forever -usepw -display :0 &
+websockify -D 80 :5900  &
 DISPLAY=:0 $IMAGEROOT/Contents/Linux-i686/bin/squeak -vm-sound-null $RESOURCES/Squeak*.image /run.st
